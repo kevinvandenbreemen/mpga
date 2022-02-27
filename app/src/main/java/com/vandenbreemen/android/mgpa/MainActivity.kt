65d1a5de95a75a5cdb8b548901c65340636c8d38
kevin <kevin.vandenbreemen@gmail.com>
2022-02-27 09:39:31 -0800

    Initial commit, Make this a library

---
+++ app/src/main/java/com/vandenbreemen/android/mgpa/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.vandenbreemen.android.mgpa
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
